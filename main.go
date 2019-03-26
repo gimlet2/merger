7e6836150bfa57e3b9a21d8743a83b8daa35014f
gimlet2 <andrey.chernishov@gmail.com>
2019-03-26 23:27:08 +0100

    init commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package merger
+
+import (
+	"errors"
+	"reflect"
+)
+
+func merge(a map[string]interface{}, b map[string]interface{}) (map[string]interface{}, error) {
+	mType := reflect.TypeOf(a)
+	for k, v := range b {
+		_, p := a[k]
+		if p && reflect.TypeOf(v) != reflect.TypeOf(a[k]) {
+			return nil, errors.New("failed to merge")
+		}
+		if p == false {
+			a[k] = v
+		}
+		if reflect.TypeOf(v) == mType && reflect.TypeOf(a[k]) == mType {
+			n, e := merge(a[k].(map[string]interface{}), v.(map[string]interface{}))
+			if e != nil {
+				return nil, e
+			}
+			a[k] = n
+		}
+
+	}
+	return a, nil
+}
